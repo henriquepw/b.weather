291223c590c262f1ab7750ed7ea32fa37c63a5c3
Henrique Miranda <enrque.ns@gmail.com>
2021-12-26 17:26:11 -0300

    chore: install nativebase

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Box, NativeBaseProvider } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NativeBaseProvider>
       <StatusBar style="auto" />
-    </View>
+      <Box flex="1" alignItems="center" justifyContent="center">Hello World</Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
