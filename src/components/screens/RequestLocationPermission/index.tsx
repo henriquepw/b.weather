7ad5e7c2b8b9915f57febae731dc2d067018d8f9
Henrique Miranda <enrque.ns@gmail.com>
2021-12-29 12:22:50 -0300

    feature: request location permission

--- src/components/screens/RequestLocationPermission/index.tsx
+++ src/components/screens/RequestLocationPermission/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Feather } from '@expo/vector-icons';
+import { useForegroundPermissions } from 'expo-location';
 import { Flex, Icon, Text } from 'native-base';
 
 import Button from '@/components/atoms/Button';
@@ -8,6 +9,23 @@ import Button from '@/components/atoms/Button';
 import ScreenContainer from '@/components/templates/ScreenContainer';
 
 const RequestLocationPermission: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [status, requestPermission] = useForegroundPermissions();
+
+  async function handleRequestPermission() {
+    setIsLoading(true);
+
+    await requestPermission();
+
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if (status?.granted) {
+      console.log(status);
+    }
+  }, [status]);
+
   return (
     <ScreenContainer>
       <Flex align="center" my="auto">
@@ -15,15 +33,22 @@ const RequestLocationPermission: React.FC = () => {
           as={Feather}
           name="alert-triangle"
           color="warning.500"
-          mb="4"
           size="12"
+          mb="4"
         />
         <Text fontSize="2xl" textAlign="center">
           É preciso da sua permissão para acessar a localização
         </Text>
       </Flex>
 
-      <Button mb="40">Conceder Permissão</Button>
+      <Button
+        mb="40"
+        onPress={handleRequestPermission}
+        isLoading={isLoading}
+        isLoadingText="Concedendo..."
+      >
+        Conceder Permissão
+      </Button>
     </ScreenContainer>
   );
 };
