1b8f2844191c9dea3ec9293d18c855cc7129e217
Henrique Miranda <enrque.ns@gmail.com>
2021-12-31 17:35:40 -0300

    test: config coverage report

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'jest-expo',
+
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFiles: ['./jest/setup.js'],
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.tsx',
+    '!src/**/*.spec.tsx',
+  ],
+  coverageReporters: ['lcov'],
+};
